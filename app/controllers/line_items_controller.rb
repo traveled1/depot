214390c90e8bf7f4cc88630a1f3ed8673d7c3531
Ed Komo <ed@gosmartr.com>
2013-02-21 18:03:09 -0800

    Finished Chapter 11 - minus the unit test

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -56,9 +56,9 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
-		  
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors,
@@ -95,3 +95,4 @@ class LineItemsController < ApplicationController
     end
   end
 end
+
